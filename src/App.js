cf0bc7bb059ca0dd751a9c49b1e9a438e7064fc7
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-12-06 02:35:52 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import QuoteForm from './components/QuoteForm'
+import Quotes from './containers/Quotes'
 
 class App extends Component {
   render() {
@@ -12,7 +14,9 @@ class App extends Component {
           TODO:
 
           Render QuoteForm and Quotes Components
-         */}
+				*/}
+				<QuoteForm />
+				<Quotes />
       </div>
     );
   }
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,35 @@
 export default (state = [], action) => {
-  return state;
+	switch (action.type) {
+		case 'ADD_QUOTE':
+			return [...state, action.quote]
+		case 'REMOVE_QUOTE':
+			let idx = state.findIndex(quote => quote.id === action.quoteId)
+			return [...state.slice(0, idx), ...state.slice(idx + 1)]
+		case 'UPVOTE_QUOTE':
+			let idx1 = state.findIndex(quote => quote.id === action.quoteId)
+			return [
+				...state.slice(0, idx1),
+				{
+					...state[idx1],
+					votes: state[idx1].votes + 1
+				},
+				...state.slice(idx1 + 1)
+			]
+		case 'DOWNVOTE_QUOTE':
+			let idx2 = state.findIndex(quote => quote.id === action.quoteId)
+			if (state[idx2].votes > 0) {
+				return [
+					...state.slice(0, idx2),
+					{
+						...state[idx2],
+						votes: state[idx2].votes - 1
+					},
+					...state.slice(idx2 + 1)
+				]
+			} else {
+				return state;
+			}
+		default:
+			return state;
+	}
 }
